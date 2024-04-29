4ec69a6ec5e8a95307bdc41aee2060df67cbea63
Apoorva N <apoorvan721@gmail.com>
2024-04-29 11:42:51 +0530

    Add Array_rotate.java file to Week 2

---
+++ Week 2/Rotate_Array.java
@@ -0,0 +1,37 @@
+package DSA.Week 2;
+
+public import java.util.Arrays;
+
+class Solution {
+    public int[] rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0, n-1);
+        reverse(nums,0, k-1);
+        reverse(nums, k, n-1); 
+        return nums;  
+    }
+    public void reverse(int nums[], int s, int e){
+        int start=s;
+        int end=e;
+        while(start<=end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }    
+    }
+}
+
+class Rotate_Array{
+    public static void main(String[] args){
+    int nums[]={1,2,3};
+    int k=4;
+    Solution s=new Solution();
+    int output[]=s.rotate(nums,k);
+    System.out.print(Arrays.toString(output));
+}
+} Rotate_Array {
+    
+}
